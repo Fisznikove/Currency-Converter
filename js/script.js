cf96892ac916d1b88b8eef6ef59a8f1e7a5978d3
Kasia Szewczyk <fisznikove@gmail.com>
2023-02-01 10:09:56 +0100

    js function added

--- js/script.js
+++ js/script.js
@@ -1,83 +1,88 @@
-let inputCurrencyElement = document.querySelector(".js-inputCurrency");
-let outputCurrencyElement = document.querySelector(".js-outputCurrency");
-let primaryElement = document.querySelector(".js-label");
-let resultElement = document.querySelector(".js-result");
-let formElement = document.querySelector(".js-form");
+{
+    const calculateResult = (currencyPair, money) => {
+        switch (currencyPair) {
+            case "EUR/USA":
+                return 1.04 * money;
 
+            case "EUR/CHF":
+                return 0.99 * money;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+            case "EUR/GBP":
+                return 0.86 * money;
 
-    let currencyPair = `${inputCurrencyElement.value}/${outputCurrencyElement.value}`;
-    let rate;
+            case "EUR/PLN":
+                return 4.70 * money;
 
+            case "USD/EUR":
+                return 0.96 * money;
 
-    switch (currencyPair) {
-        case "EUR/USA":
-            rate = 1.04;
-            break;
-        case "EUR/CHF":
-            rate = 0.99;
-            break;
-        case "EUR/GBP":
-            rate = 0.86;
-            break;
-        case "EUR/PLN":
-            rate = 4.70;
-            break;
-        case "USD/EUR":
-            rate = 0.96;
-            break;
-        case "USD/CHF":
-            rate = 0.95;
-            break;
-        case "USD/GBP":
-            rate = 0.83;
-            break;
-        case "USD/PLN":
-            rate = 4.51;
-            break;
-        case "CHF/EUR":
-            rate = 1.02;
-            break;
-        case "CHF/USD":
-            rate = 1.06;
-            break;
-        case "CHF/GBP":
-            rate = 0.87;
-            break;
-        case "CHF/PLN":
-            rate = 4.77;
-            break;
-        case "GBP/EUR":
-            rate = 1.16;
-            break;
-        case "GBP/USD":
-            rate = 1.21;
-            break;
-        case "GBP/CHF":
-            rate = 1.14;
-            break;
-        case "GBP/PLN":
-            rate = 5.45;
-            break;
-        case "PLN/EUR":
-            rate = 0.21;
-            break;
-        case "PLN/USD":
-            rate = 0.22;
-            break;
-        case "PLN/CHF":
-            rate = 0.21;
-            break;
-        case "PLN/GBP":
-            rate = 0.18;
-            break;
-        default:
-            rate = 1;
+            case "USD/CHF":
+                return 0.95 * money;
+
+            case "USD/GBP":
+                return 0.83 * money;
+
+            case "USD/PLN":
+                return 4.51 * money;
+
+            case "CHF/EUR":
+                return 1.02 * money;
+
+            case "CHF/USD":
+                return 1.06 * money;
+
+            case "CHF/GBP":
+                return 0.87 * money;
+
+            case "CHF/PLN":
+                return 4.77 * money;
+
+            case "GBP/EUR":
+                return 1.16 * money;
+
+            case "GBP/USD":
+                return 1.21 * money;
+
+            case "GBP/CHF":
+                return 1.14 * money;
+
+            case "GBP/PLN":
+                return 5.45 * money;
+
+            case "PLN/EUR":
+                return 0.21 * money;
+
+            case "PLN/USD":
+                return 0.22 * money;
+
+            case "PLN/CHF":
+                return 0.21 * money;
+
+            case "PLN/GBP":
+                return 0.18 * money;
+
+            default:
+                return 1 * money;
+        }
+
     }
-    let money = primaryElement.value;
-    let result = money * rate;
-    resultElement.innerText = `${result.toFixed(2)} ${outputCurrencyElement.value}`;
 
-});
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const inputCurrencyElement = document.querySelector(".js-inputCurrency");
+            const outputCurrencyElement = document.querySelector(".js-outputCurrency");
+            const primaryElement = document.querySelector(".js-label");
+            const resultElement = document.querySelector(".js-result");
+            const currencyPair = `${inputCurrencyElement.value}/${outputCurrencyElement.value}`;
+            const money = +primaryElement.value
+            const result = calculateResult(currencyPair, money);
+            resultElement.innerText = `${result.toFixed(2)} ${outputCurrencyElement.value}`;
+
+        });
+    }
+
+    init();
+
+}
